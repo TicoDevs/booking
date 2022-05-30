d3315ee239ca3dc6e4a594ff6970b3c17db6e759
aporras <aoporrasro@est.utn.ac.cr>
2022-05-29 20:55:22 -0600

    Initial commit

---
+++ src/main/java/org/mfc/booking/MfcBookingApplication.java
@@ -0,0 +1,13 @@
+package org.mfc.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MfcBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MfcBookingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/mfc/booking/MfcBookingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.mfc.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MfcBookingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
